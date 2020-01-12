a6d83428a740b7de38e0f4aab2ccc1e172d3cf50
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-12 16:57:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,9 @@
-def square_array(results)
-  re
-  while results[ *2] do
-    results.push
-  puts results
+def square_array(array)
+  results = []
+  counter = 0
+  while counter < array.size do
+    results.push(array[counter]**2)
+    counter += 1
+  end
+  results
 end
